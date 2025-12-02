af14fb6fda0e64a47519b285c503ddc1b193396a
Tuannnnday <tuantrieusontay@gmail.com>
2025-12-02 18:21:30 +0700

    Xong ql gv

---
+++ QLTruongCap3/Models/DiemDanhItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLTruongCap3.Models
+{
+    public class DiemDanhItem
+    {
+        public string MAHS { get; set; }
+        public string TRANGTHAI { get; set; } // Có mặt, Vắng có phép...
+        public string GHICHU { get; set; }    // Lý do vắng
+
+
+    }
+}
